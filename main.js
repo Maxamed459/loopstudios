00b688d2d823f0bc5e37871229e8512a1147c004
Maxamed459 <maxamedmahdi459@gmail.com>
2025-02-01 14:14:29 +0300

    initial commit!

---
+++ main.js
@@ -0,0 +1,7 @@
+let navDiolog = document.getElementById("navDiolog");
+function navHandler() {
+    navDiolog.classList.toggle('hidden');
+}
+const hideMenu = () => {
+    navDiolog.classList.toggle('hidden');
+}
